8834372af17a9e91861f3436646f8932df7321cd
rdw5286 <rdw5286@psu.edu>
2020-02-26 17:53:02 -0500

    Test Cloning Commit

---
+++ JavaFP/src/javafp/JavaFP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafp;
+
+/**
+ *
+ * @author hello
+ */
+public class JavaFP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 5;
+    }
+    
+}
